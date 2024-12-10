738a4da510fdfc64a7e6d0c80455e2c58af42d97
kimura08a <168007937+kimura08a@users.noreply.github.com>
2024-12-10 12:02:20 +0900

    modified Scanner.java identifier

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -13,6 +13,27 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -74,13 +95,54 @@ class Scanner {
 
       case '"': string();break;
 
+      case 'o':
+  if (match('r')) {
+    addToken(OR);
+  }
+  break;
+
       default:
       if (isDigit(c)) {
         number();
+      } else if (isAlpha(c)) {
+        identifier();
       } else {
         Lox.error(line, "Unexpected character.");
       }
     }
+}
+
+private void identifier() {
+  while (isAlphaNumeric(peek())) advance();
+  String text = source.substring(start, current);
+  TokenType type = keywords.get(text);
+  if (type == null) type = IDENTIFIER;
+  addToken(type);
+}
+
+private boolean isAlpha(char c) {
+  return (c >= 'a' && c <= 'z') ||
+         (c >= 'A' && c <= 'Z') ||
+          c == '_';
+}
+
+private boolean isAlphaNumeric(char c) {
+  return isAlpha(c) || isDigit(c);
+}
+
+private void number() {
+  while (isDigit(peek())) advance();
+
+  // Look for a fractional part.
+  if (peek() == '.' && isDigit(peekNext())) {
+    // Consume the "."
+    advance();
+
+    while (isDigit(peek())) advance();
+  }
+
+  addToken(NUMBER,
+      Double.parseDouble(source.substring(start, current)));
 }
 
   private boolean match(char expected) {
@@ -134,21 +196,6 @@ class Scanner {
   private boolean isDigit(char c) {
     return c >= '0' && c <= '9';
   } 
-
-  private void number() {
-    while (isDigit(peek())) advance();
-
-    // Look for a fractional part.
-    if (peek() == '.' && isDigit(peekNext())) {
-      // Consume the "."
-      advance();
-
-      while (isDigit(peek())) advance();
-    }
-
-    addToken(NUMBER,
-        Double.parseDouble(source.substring(start, current)));
-  }
 
   private char peekNext() {
     if (current + 1 >= source.length()) return '\0';
